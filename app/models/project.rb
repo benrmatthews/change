683dfc6688dcdc1b5fa31ab0c7d8dad69feea9e5
Ben Matthews <ben@montfort.io>
2017-12-27 19:33:13 +0000

    added search form

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,12 @@ class Project < ApplicationRecord
   	params.require(:user).permit(:name, :website, :info, :area, :tag_list)
   end	
   acts_as_taggable_on :tags
+
+  def self.search(term)
+	if term
+	  where('name ILIKE ?', "%#{term}%")
+	else
+	  all
+	end
+  end
 end
